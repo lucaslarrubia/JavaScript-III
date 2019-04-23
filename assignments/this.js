c04337da970ba2f7d2bde0f5b31f7cedb6e7d050
Lucas Larrubia <Lucas@Marcias-iMac.local>
2019-04-23 18:23:07 -0400

    answered questions for the four principles of the 'this' keyword.

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
+* in your own words. explain the four principles for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When in the global scope, the value of “this” will be the window/console Object which is essentially JavaScript. 
+* 2. Implicit binding is the most common application of the 'this' keyword. 'This' refers to the object called before the dot in a function.
+* 3. New binding is essentially creating a new object inside of a constructor function. 
+* 4. Explicit binding is using methods such as .call or .apply 
 *
 * write out a code example of each explanation above
 */
